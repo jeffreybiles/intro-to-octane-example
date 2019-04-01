130e06254c98902cf182cd4234e92ddfab9d9a8d
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-01 15:41:26 -0500

    highlighting the selected column

--- app/components/sortable-header.js
+++ app/components/sortable-header.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class SortableHeaderComponent extends Component {
+  get isSelected(){
+    return this.args.sortBy === this.args.sortedBy;
+  }
 }
