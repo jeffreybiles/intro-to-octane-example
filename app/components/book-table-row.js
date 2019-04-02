8c00dbe10ef94ffa62be8171cd8b2b91e5fbea06
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-02 01:43:38 -0500

    zebra striping

--- app/components/book-table-row.js
+++ app/components/book-table-row.js
@@ -4,4 +4,8 @@ export default class BookTableRowComponent extends Component {
   get isSelected(){
     return this.args.book === this.args.selectedBook;
   }
+
+  get isStriped(){
+    return this.args.index % 2 === 1
+  }
 }
