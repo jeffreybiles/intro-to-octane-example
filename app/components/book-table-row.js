e31b506e99f8cd4d0020c625db9d8a22ea9ca78f
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-01 23:24:01 -0500

    highlighting the rows

--- app/components/book-table-row.js
+++ app/components/book-table-row.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class BookTableRowComponent extends Component {
+  get isSelected(){
+    return this.args.book === this.args.selectedBook;
+  }
 }
